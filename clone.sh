721c5cf511274dbe7d77fcef35ab5c0ba467168b
Rajkale99 <kaleraj.rk@gmail.com>
2021-07-10 01:14:49 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir .repo/local_manifests
-curl --create-dirs -L -o .repo/local_manifests/xiaomi_sdm845_default.xml -O -L https://raw.githubusercontent.com/Hlcpereira/local_manifests/master/xiaomi_sdm845_default.xml >/dev/null  2>&1
+curl --create-dirs -L -o .repo/local_manifests/local_manifests.xml -O -L https://raw.githubusercontent.com/rajkale99/local_manifest/main/local_manifest >/dev/null  2>&1
