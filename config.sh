1d65f4847f20116d2a6b82f8d7e23650e81934ef
Rajkale99 <kaleraj.rk@gmail.com>
2021-07-10 01:12:43 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-GITHUB_USER=Hlcpereira
-GITHUB_EMAIL=pereirah2001@gmail.com
+GITHUB_USER=rajkale99
+GITHUB_EMAIL=kaleraj.rk@gmail.com
 
-KBUILD_BUILD_USER="Hlcpereira"
-KBUILD_BUILD_HOST="Somewhere"
+KBUILD_BUILD_USER="Rajkale99"
+KBUILD_BUILD_HOST="LegionOS"
 
-ROM="PixelExperience"
-manifest_url="https://github.com/PixelExperience/manifest"
-export rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
-branch="pie"
+ROM="ProjectLegionOS"
+manifest_url="https://github.com/Project-LegionOS/manifest"
+export rom_vendor_name="legion" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
+branch="11"
 
-release_repo="Hlcpereira/rom_builder"
+release_repo="rajkale99/rom_builder"
 
-timezone="America/Sao_Paulo"
+timezone="Asia/Kolkata"
